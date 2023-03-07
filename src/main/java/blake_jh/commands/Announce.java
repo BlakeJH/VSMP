2423d400b30961003366dd46644bd5493eb6c3c7
BlakeJH <blakejenkinshanks@gmail.com>
2023-03-06 19:18:13 -0700

    Revisions

---
+++ src/main/java/blake_jh/commands/Announce.java
@@ -0,0 +1,28 @@
+package blake_jh.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Announce implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 0) {
+            sender.sendMessage(ChatColor.RED + "Usage: /announce <message>");
+            return true;
+        }
+
+        StringBuilder message = new StringBuilder();
+        for (String arg : args) {
+            message.append(arg).append(" ");
+        }
+
+        // Send message to all players
+        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', "&4&lANNOUNCEMENT:&r " + message.toString().trim()));
+
+        return true;
+    }
+}
